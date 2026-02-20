40ce0b03d024b9861c5e106f12fb0ac4a832f0d8
UltraMagnusss <mahmudi200102@gmail.com>
2026-02-20 17:46:23 +0500

    Подключили базу данных и залили туда все подсказки и ответы к ним, следующим шагом будет убратьь лишние строки кода и заменить их  на данные из БД

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,14 +1,15 @@
 package Game
 
 import (
-	"fmt"
+	"strings"
 	"wordGame/internal/data"
 )
 
 type Game struct {
-	Score int
-	Lives int
-	Words []data.Word
+	Score        int
+	Lives        int
+	Words        []data.Word
+	CurrentIndex int
 }
 
 func NewGame() *Game {
@@ -20,51 +21,79 @@ func NewGame() *Game {
 	return &game
 }
 
-func (g *Game) Run() {
-	fmt.Println("Welcome to the game")
-	fmt.Printf("Guess the hidden word by the given hint\n")
-	fmt.Printf(">>>>>Rules<<<<<\n")
-	fmt.Printf("1. You have 5 attempts.\n2. Find all words to win.\n3. Have fun and be smart.\n\n")
-
-	for _, word := range g.Words {
-		if g.Lives == 0 {
-			fmt.Println("You lost!")
-			break
-		}
-		hint := word.Hint
-		answer := word.Answer
-
-		fmt.Println("Hint:", hint)
-
-		for g.Lives > 0 {
-			var userAnswer string
-			fmt.Println("\nEnter your answer")
-			fmt.Scan(&userAnswer)
-
-			if userAnswer == answer {
-				g.Score++
-				fmt.Printf("\nCorrect! Your score for this word: 1\n")
-				fmt.Printf("Total score: %v\n", g.Score)
-				fmt.Printf("Remaining attempts: %v\n", g.Lives)
-				fmt.Println()
-				break
-			} else {
-				fmt.Println("Hint:", hint)
-				g.Lives--
-				if g.Lives > 0 {
-					fmt.Printf("\nWrong answer, Try again!\n")
-					fmt.Printf("\nRemaining attempts: %v\n", g.Lives)
-				} else {
-					fmt.Println("You lost!")
-					fmt.Printf("The correct answer was: %v\n", answer)
-					return
-				}
-
-			}
-		}
+func (g *Game) GetCurrentHint() string {
+	if g.CurrentIndex >= len(g.Words) {
+		return ""
 	}
+	return g.Words[g.CurrentIndex].Hint
 
-	if g.Lives > 0 {
-		fmt.Printf("Congratulations! You guessed all words. \nFinal score: %v\n", g.Score)
-	}
 }
+
+func (g *Game) CheckAnswer(userAnswer string) bool {
+	correctAnswer := g.Words[g.CurrentIndex].Answer
+	if g.IsGameOver() {
+		return false
+	}
+
+	if strings.TrimSpace(strings.ToLower(userAnswer)) == strings.ToLower(correctAnswer) {
+		g.Score++
+		return true
+	} else {
+		g.Lives--
+		return false
+	}
+
+}
+
+func (g *Game) IsGameOver() bool {
+	return g.Lives <= 0 || g.CurrentIndex >= len(g.Words)
+}
+
+// func (g *Game) Run() {
+// 	fmt.Println("Welcome to the game")
+// 	fmt.Printf("Guess the hidden word by the given hint\n")
+// 	fmt.Printf(">>>>>Rules<<<<<\n")
+// 	fmt.Printf("1. You have 5 attempts.\n2. Find all words to win.\n3. Have fun and be smart.\n\n")
+
+// 	for _, word := range g.Words {
+// 		if g.Lives == 0 {
+// 			fmt.Println("You lost!")
+// 			break
+// 		}
+// 		hint := word.Hint
+// 		answer := word.Answer
+
+// 		fmt.Println("Hint:", hint)
+
+// 		for g.Lives > 0 {
+// 			var userAnswer string
+// 			fmt.Println("\nEnter your answer")
+// 			fmt.Scan(&userAnswer)
+
+// 			if userAnswer == answer {
+// 				g.Score++
+// 				fmt.Printf("\nCorrect! Your score for this word: 1\n")
+// 				fmt.Printf("Total score: %v\n", g.Score)
+// 				fmt.Printf("Remaining attempts: %v\n", g.Lives)
+// 				fmt.Println()
+// 				break
+// 			} else {
+// 				fmt.Println("Hint:", hint)
+// 				g.Lives--
+// 				if g.Lives > 0 {
+// 					fmt.Printf("\nWrong answer, Try again!\n")
+// 					fmt.Printf("\nRemaining attempts: %v\n", g.Lives)
+// 				} else {
+// 					fmt.Println("You lost!")
+// 					fmt.Printf("The correct answer was: %v\n", answer)
+// 					return
+// 				}
+
+// 			}
+// 		}
+// 	}
+
+// 	if g.Lives > 0 {
+// 		fmt.Printf("Congratulations! You guessed all words. \nFinal score: %v\n", g.Score)
+// 	}
+// }
---
+++ pkg/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+var DB *sql.DB
+
+func DataBase() {
+	dsn := "postgres://postgres:newpasword@localhost:5432/wordsdb?sslmode=disable"
+	var err error
+
+	DB, err = sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
